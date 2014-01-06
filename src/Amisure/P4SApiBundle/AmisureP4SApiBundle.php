364d9fd514b385c80f6ac4f2249786565ccfaa9d
Olivier Maridat <olivier.maridat@trialog.com>
2014-01-06 15:32:03 +0000

    Fix: don't launch the HWIOAuthBundle anymore

--- src/Amisure/P4SApiBundle/AmisureP4SApiBundle.php
+++ src/Amisure/P4SApiBundle/AmisureP4SApiBundle.php
@@ -5,6 +5,6 @@ namespace Amisure\P4SApiBundle;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 use HWI\Bundle\OAuthBundle\HWIOAuthBundle;
 
-class AmisureP4SApiBundle extends HWIOAuthBundle
+class AmisureP4SApiBundle extends Bundle
 {
 }
